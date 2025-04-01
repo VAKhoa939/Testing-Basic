bb0948498e05023468571be50b87abaa6d65d533
Kaos <21110046@student.hcmute.edu.vn>
2025-04-01 17:20:59 +0700

    Finished writing the last 3 tasks (4 -> 6)

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,5 +1,88 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.nullValue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
-class TasksViewModelTest
+@Config(sdk = [30]) // Remove when Robolectric supports SDK 31
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Subject under test
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+        /**
+         * Use this line when not having setup method
+         */
+//        // Given a fresh TasksViewModel
+//        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        /**
+         * Observer Forever version
+         */
+//        // Create observer - no need for it to do anything!
+//        val observer = Observer<Event<Unit>> {}
+//
+//        try {
+//            // Observe the LiveData forever
+//            tasksViewModel.newTaskEvent.observeForever(observer)
+//
+//            // When adding a new task
+//            tasksViewModel.addNewTask()
+//
+//            // Then the new task event is triggered
+//            val value = tasksViewModel.newTaskEvent.value
+//            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+//        } finally {
+//            // Whatever happens, don't forget to remove the observer!
+//            tasksViewModel.newTaskEvent.removeObserver(observer)
+//        }
+
+        /**
+         * Observer util file version
+         */
+        // When adding a new task
+        tasksViewModel.addNewTask()
+
+        // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        /**
+         * Use this line when not having setup method
+         */
+//        // Given a fresh ViewModel
+//        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        // When the filter type is ALL_TASKS
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the "Add task" action is visible
+        assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
+    }
+}
